9e197696123af192b9d201670c6bc8b8eba76436
jaehongjun <gatsby0391@naver.com>
2018-07-15 14:23:58 +0900

    밑그림

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,35 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { ChartPage } from '../pages/chart/chart';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LocatePage } from '../pages/locate/locate';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { KakaoMapsModule } from 'kakao-maps-sdk';
+
+import { HttpClient } from '@angular/common/http';
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ChartPage,
+    LocatePage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    KakaoMapsModule,
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +38,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ChartPage,
+    LocatePage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    HttpClient,
   ]
 })
 export class AppModule {}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { ChartPage } from '../chart/chart';
+import { LocatePage } from '../locate/locate';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,9 +12,9 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab2Root = LocatePage;
   tab3Root = ContactPage;
-
+  tab4Root = ChartPage;
   constructor() {
 
   }
